0c16048ec69beac2317faf3e86f0fcaf7c247be1
andrewimpellitteri <andrewimpellitteri@gmail.com>
2025-09-30 13:42:35 -0400

    work on #34, need to double check to be able to close out issue, and minor style tweaks

---
+++ static/js/dropdown-data.js
@@ -0,0 +1,29 @@
+// Materials
+const materialValues = [
+  "Acrylic", "Dacron", "Vinyl", "Canvas", "Pyrotone",
+  "A+V", "V(+A)", "A+V+Screen", "Cotton", "Mylar",
+  "Seamark", "A coated Poly", "Mesh", "Stamoid",
+  "A+Screen", "CoatedA", "Stam+V", "Berber", "Xdrive",
+  "A+Vmesh", "Vmesh"
+];
+
+// Descriptions
+const descriptionValues = [
+  "Patio", "Curtain", "Single", "Umbrella", "Main",
+  "Yankee", "Staysail", "Genoa", "Spinnaker", "Sail",
+  "Double", "Extension", "Awning", "Dodger", "Bimini",
+  "Cover", "Cushion", "Bag", "Jib", "Lateral", "Valance",
+  "Window", "Roller", "Sailcover", "Boltrope", "Connector",
+  "Code 0", "Mizzen", "Freestander", "Cockpit cover",
+  "Bow cover", "Mooring cover", "Passagemaker", "Gennaker",
+  "Pergola cover", "Patio + backflap"
+];
+
+// Colors
+const colorValues = [
+  "White", "Blue", "Red", "Green", "Green/White", "Yellow",
+  "Off White", "Tan", "Beige", "Navy", "Taupe", "Pacific Blue",
+  "Grey", "Black", "Gold", "Toast", "Burgundy", "Silver", "Linen",
+  "Oyster", "Parchment", "Brown", "UK Yellow", "North Blue",
+  "Doyle Grey", "Npryde Grey", "Patchwork", "Orange", "Purple"
+];
