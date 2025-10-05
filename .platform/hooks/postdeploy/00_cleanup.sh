ce4c47fedcf98756a14d20c7e25b6d043df03020
andrewimpellitteri <andrewimpellitteri@gmail.com>
2025-10-05 14:33:51 -0400

    Clean rebuild of .platform directory without .gitignore files

--- .platform/hooks/postdeploy/00_cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Remove .gitignore from hooks directory - EB tries to execute it as a script
-rm -f /var/app/current/.platform/hooks/postdeploy/.gitignore
-echo "Cleanup: Removed .gitignore from postdeploy hooks"
